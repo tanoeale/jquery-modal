8f47fecfdbdcfcebfdb975101f8921154d795c7c
christian.raneback@gmail.com <christian.raneback@gmail.com@df7d5d78-f47f-a8cb-93da-4c210a50ac46>
2011-03-21 10:28:03 +0000

    - Modal follow body scroll

--- trunk/jquery.modal.js
+++ trunk/jquery.modal.js
@@ -1,107 +1,114 @@
 /**
-*	@name							Modal
-*	@descripton						Modal creates modals
-*	@version						1.1
-*	@requires						Jquery 1.4.2
+*    @name                            Modal
+*    @descripton                        Modal creates modals
+*    @version                        1.0
+*    @requires                        Jquery 1.4.2
 *
-*	@author							Jan Jarfalk
-*	@author-email					jan.jarfalk@unwrongest.com
-*	@author-website					http://www.unwrongest.com
+*    @author                            Jan Jarfalk
+*    @author-email                    jan.jarfalk@unwrongest.com
+*    @author-website                    http://www.unwrongest.com
 *
-*	@licens							MIT License - http://www.opensource.org/licenses/mit-license.php
+*    @licens                            MIT License - http://www.opensource.org/licenses/mit-license.php
 */
 
 (function($) {
-	
-	$.modal = function() {
-	
-		var callback 	= callback || function(){},
-			$container	= $('#modal-wrap'),
-			$document	= $(document),
-			$overlay	= $('#modal-overlay');
-				
-		$.modal.empowerLinks = function() {
-			
-			$('a[rel*=modal]').live('click', function( event ){
+    
+    $.modal = function() {
+    
+        var callback     = callback || function(){},
+            $container    = $('#modal-wrap'),
+            $document    = $(document),
+            $overlay    = $('#modal-overlay');
+                
+        $.modal.empowerLinks = function() {
+            
+            $('a[rel*=modal]').live('click', function( event ){
 
-				var $this = $(this);
-				var modal = $this.attr('href');
-				
-				$.modal.open(modal);
-								
-			});
-			
-		}
+                var $this = $(this);
+                var modal = $this.attr('href');
+                
+                $.modal.open(modal);
+                                
+            });
+            
+        }
 
-		$.modal.open = function(modal) {
-			
-			$('.modal', $container).hide();
-			var $modal;
-			
-			if(modal.match(/#/)){
-				$modal = $(modal);
-				show();	
-			} else {
-				$.get(modal, function( element ){
-					$modal = $(element);
-					$overlay.after($modal);
-					show();
-				});
-			}
-			
-			function show(){
-			if(!$modal.length) {
-				return false;
-			}
-			
-			$container.show();
-			$modal.show();
-			
-			// Make modal fit
-			$overlay.height('');
-			if( $modal.height() > $overlay.height() ){
-				$overlay.height($modal.outerHeight() + 200);
-			}
-			}
-			
-				
-				
-			$document.bind( 'keypress.modal' , function( event ){
-				if( event.keyCode === 27 ){
-					$.modal.close();
-				}
-			});
-				
-			$container.live('click.modal', function( event ){
-				
-				if(event.target.className == 'overlay' || event.target.className == 'modal-close'){
-					event.preventDefault();
-					$.modal.close();
-				}
-				
-			});
-		}
-		
-		$.modal.close = function( index, item ) {
-		
-			$document.unbind( 'keypress.modal' );
-			$container.unbind( 'click.modal' );				
-			$('.modal', $container).hide();
-			$container.hide();
-			
-		}
-		// Hide all modals
-		$('.modal', $container).hide();
-		
-		// Open current modal window
-		if(location.hash.length > 0) {
-			$.modal.open(location.hash);
-		}
-		
-		// Convert links to hashes.
-		$.modal.empowerLinks();
+        $.modal.open = function(modal) {
+            
+            $('.modal', $container).hide();
+            var $modal;
+            
+            if(modal.match(/#/)){
+                $modal = $(modal);
+                show();    
+            } else {
+                $.get(modal, function( element ){
+                    $modal = $(element);
+                    $overlay.after($modal);
+                    show();
+                });
+            }
+            
+            function show(){
+                //If modal doesnt exists...break
+                if(!$modal.length) {
+                    return false;
+                }
+                
+                //Opening where you are (looking at body scroll)
+                $modal.css('top', $('html').scrollTop());
+                
+                //Show overlay and the modal
+                $container.show();
+                $modal.show();
+                
+        
+                // Make modal fit
+                /*
+                if( $modal.height() > $overlay.height() ){
+                    $overlay.height($modal.outerHeight());
+                }
+                */
+            }
+            
+                
+                
+            $document.bind( 'keypress.modal' , function( event ){
+                if( event.keyCode === 27 ){
+                    $.modal.close();
+                }
+            });
+                
+            $container.live('click.modal', function( event ){
+                
+                if(event.target.className == 'overlay' || event.target.className == 'modal-close'){
+                    event.preventDefault();
+                    $.modal.close();
+                }
+                
+            });
+        }
+        
+        $.modal.close = function( index, item ) {
+        
+            $document.unbind( 'keypress.modal' );
+            $container.unbind( 'click.modal' );                
+            $('.modal', $container).hide();
+            $container.hide();
+            
+        }
+        // Hide all modals
+        $('.modal', $container).hide();
+        
+        // Open current modal window
+        if(location.hash.length > 0) {
+            $.modal.open(location.hash);
+        }
+        
+        // Convert links to hashes.
+        $.modal.empowerLinks();
 
-	}
+    }
 
 
 })(jQuery);

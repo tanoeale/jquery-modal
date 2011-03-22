b92f4d02acabcdab7d677eb060210a98ccf7f43a
christian.raneback@gmail.com <christian.raneback@gmail.com@df7d5d78-f47f-a8cb-93da-4c210a50ac46>
2011-03-22 14:46:54 +0000

    - Commented the code - Added fix for ie7 - Removed rel attributed.. something users can write themself. Smaller js file...

--- trunk/jquery.modal.js
+++ trunk/jquery.modal.js
@@ -14,30 +14,20 @@
 (function($) {
     
     $.modal = function() {
-    
+    	
+    	//Settings
         var callback     = callback || function(){},
             $container    = $('#modal-wrap'),
             $document    = $(document),
             $overlay    = $('#modal-overlay');
-                
-        $.modal.empowerLinks = function() {
-            
-            $('a[rel*=modal]').live('click', function( event ){
-
-                var $this = $(this);
-                var modal = $this.attr('href');
-                
-                $.modal.open(modal);
-                                
-            });
-            
-        }
-
+               
+		//Open function
         $.modal.open = function(modal) {
-            
+            //Hide all the modals
             $('.modal', $container).hide();
             var $modal;
             
+            //Checks if it is a anchor link or an normal link. If its normal open with ajax (get)
             if(modal.match(/#/)){
                 $modal = $(modal);
                 show();    
@@ -50,54 +40,84 @@
             }
             
             function show(){
-            
+            	//Check if a model exists
                 if(!$modal.length) {
                     return false;
                 }
-                $modal.css('top', $(window).scrollTop());
                 
+                //Place the modal where you are
+                fixScroll();
+                
+                //Show container and modal
                 $container.show();
                 $modal.show();
-    
-            }
-            
-			$(window).resize(function () {
-				$modal.css('top', $(window).scrollTop());
-			});
-
                 
+                // :( Ie 7 fix
+                if($.browser.msie && $.browser.version < 8.0) {
+	                setTimeout(function () {
+	                	$(window).scrollTop($modal.offset().top)
+	                }, 1);
+                } 
+                
+               
+            }
+			
+			//Move the modal to where you are according to scroll window.
+			function fixScroll() {
+                if(!$modal.length) {
+                    return false;
+                }
+                
+                var scrollTop = $(window).scrollTop();
+               	$modal.css('top', scrollTop);
+               	
+			}
+			
+			//If you change window, fix the modal so it doesnt follow it (like closing firebug for ff)
+			$(window).bind('resize', fixScroll);
+			
+			//Close with esc key (27)
             $document.bind( 'keypress.modal' , function( event ){
+            
                 if( event.keyCode === 27 ){
                     $.modal.close();
                 }
+                
             });
-                
+            
+            //Close when clicking on overlay    
             $container.live('click.modal', function( event ){
-                
+            
                 if(event.target.className == 'overlay' || event.target.className == 'modal-close'){
                     event.preventDefault();
                     $.modal.close();
                 }
                 
-            });
+            });        
         }
         
+        //Close function
         $.modal.close = function( index, item ) {
+	        //Changes the hash to closed
         	window.location.hash = 'closed';
+      		
+      		//Unbind events  	
             $document.unbind( 'keypress.modal' );
-            $container.unbind( 'click.modal' );                
+            $container.unbind( 'click.modal' );
+            
+            //Hide modal, and container             
             $('.modal', $container).hide();
             $container.hide();
             
         }
-
+		
+		//Hide any modal, thats open.
         $('.modal', $container).hide();
         
+        //If you have any hash open it
         if(location.hash.length > 0) {
             $.modal.open(location.hash);
         }
-        
-        $.modal.empowerLinks();
 
     }
 
